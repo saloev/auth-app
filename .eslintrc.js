7d8ba90bd3e3b751a42459f363e1d64981dfee78
saloev <saloev.saadi@yandex.ru>
2020-02-28 01:52:45 +0300

    fix: production public path

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,5 @@ module.exports = {
     parser: 'babel-eslint',
   },
 
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-
   extends: ['plugin:vue/essential', '@vue/airbnb', 'eslint:recommended', '@vue/prettier'],
 }
--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ prod:
 	npm run build
 
 lint: 
-	npm run lint .
+	npm run lint ./src
 
 lint-fix: 
-	npm run lint . --fix
+	npm run lint ./src/ --fix
